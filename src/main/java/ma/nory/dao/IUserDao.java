6a610214f69e55976af1c4983f1a78d0ada44194
chater <chater.yassine@gmail.com>
2019-11-25 15:43:10 +0100

    format dao

--- src/main/java/ma/nory/dao/IUserDao.java
+++ src/main/java/ma/nory/dao/IUserDao.java
@@ -4,21 +4,19 @@ import java.util.List;
 
 import ma.nory.models.User;
 
-
 public interface IUserDao {
-	
+
 	abstract void addUser(User user);
-	
+
 	abstract void updateUser(User user);
-	
+
 	abstract void deleteUser(User user);
-	
+
 	abstract User findUser(Integer userId);
-	
+
 	abstract User findUser(String username);
-	
+
 	abstract List<User> getAllUsers();
-	
+
 	abstract void addNewAccount(User user);
-}	
-	
+}
